0144ec4017b2712e72912ab3e2a92e4916fa8830
Krishnaarunangsu <arunangsutech@gmail.com>
2024-09-29 00:51:54 +0530

    first commit

---
+++ src/com/java/docker/tut/Hello.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.java.docker.tut;
+
+/**
+ * 
+ */
+public class Hello {
+
+	/**
+	 * 
+	 */
+	public Hello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a Java App \n by using docker");
+
+	}
+
+}
