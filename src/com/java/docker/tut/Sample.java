dd7759df1a0a276cdc1c17bd0f1a86a4b8536c48
Krishnaarunangsu <arunangsutech@gmail.com>
2024-10-05 22:03:49 +0530

    Added Comment

--- src/com/java/docker/tut/Sample.java
+++ src/com/java/docker/tut/Sample.java
@@ -2,6 +2,8 @@
 package com.java.docker.tut;
 
 class Sample{
+
+    //Sample Class for Docker
      public static void main(String args[]){
          System.out.println("Welcome to GeeksForGeeks");
      }
